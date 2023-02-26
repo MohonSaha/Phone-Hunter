52dec96b5680da22e79cb97592f24a7eeda9970a
MohonSaha <practicewithmohon@gmail.com>
2023-02-26 11:21:19 +0600

    Show and hide loading spinner while loading API data

--- JS/app.js
+++ JS/app.js
@@ -39,13 +39,28 @@ const displaPhones = (phones) =>{
         `;
         phonesContainer.appendChild(phoneDiv);
     });
+    //Start spinner or loader
+    toggleSpinner(false);
 }
 
 
 document.getElementById('btn-search').addEventListener('click', function(){
+    //Start spinner or loader
+    toggleSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     loadPhone(searchText);
 })
+
+
+const toggleSpinner = isLoading => {
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none');
+    }
+    else{
+        loaderSection.classList.add('d-none');
+    }
+}
 
 // loadPhone()
